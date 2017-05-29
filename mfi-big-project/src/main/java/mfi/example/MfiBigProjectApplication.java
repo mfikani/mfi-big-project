bf569b22980ae76566757efb3aacfe8fc3386df6
mfi <miled.fikani@gmail.com>
2017-05-29 15:45:37 +0300

    first commit : project structure

---
+++ mfi-big-project/src/main/java/mfi/example/MfiBigProjectApplication.java
@@ -0,0 +1,12 @@
+package mfi.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfiBigProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfiBigProjectApplication.class, args);
+	}
+}
